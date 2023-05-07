ffd8d40e85347d7b88fb9555c72eb310d861eeac
Adrian Gabriel <90657835+AdrianGKS@users.noreply.github.com>
2023-05-07 16:56:56 -0300

    Payment, one-to-one association

--- src/main/java/com/example/projetospringbootwebservice/entities/Order.java
+++ src/main/java/com/example/projetospringbootwebservice/entities/Order.java
@@ -18,14 +18,20 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+
     @ManyToOne
     @JoinColumn(name = "clientId")
     private User client;
     private Integer status;
+
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
     }
@@ -72,6 +78,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
